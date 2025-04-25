fca690ab79382392ece1e4c7fbb87ad9d2d5c5f8
dk0509 <agrawaldevansh2005@gmail.com>
2025-04-25 12:39:45 +0530

    react updation

--- reactregistration/src/components/Login.jsx
+++ reactregistration/src/components/Login.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
+
+  async function sendData(e){
+    e.preventDefault();
+    
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    alert(email+password);
+        const response=await fetch("https://itcstudentapp-backend-8dfm.onrender.com",{
+          method:'POST',
+          body:JSON.stringify({email,password}),
+          headers:{'Content-Type':'application/json'}
+        })
+        const res=await response.json();
+        alert(res.msg);
+  }
   return (
     <div>Login
-        <div>
-        <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<div>
+<form onSubmit={sendData}>
+      
+  <div>
+    <label>Email address</label>
+    <input type="email" name='email' id="email1" aria-describedby="emailHelp" placeholder="Enter email" />
+  
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+  <div>
+    <label>Password</label>
+    <input type="password" name='password'  id="pass1" placeholder="Password" />
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Login</button>
+  
+  <button type="submit">Login</button>
 </form>
-        </div>
+
+</div>
+
     </div>
   )
 }
--- reactregistration/src/components/Registration.jsx
+++ reactregistration/src/components/Registration.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const Registration = () => {
+function Registration() {
+  async function sendData(e){
+    e.preventDefault();
+    const name=e.target.name.value;
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    alert(name+email+password);
+        const response=await fetch("https://itcstudentapp-backend-8dfm.onrender.com",{
+          method:'POST',
+          body:JSON.stringify({name,email,password}),
+          headers:{'Content-Type':'application/json'}
+        })
+        const res=await response.json();
+        alert(res.msg);
+  }
   return (
     <div>Registration
-        <div>
-        <form>
-        <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
-    <input type="name" class="form-control" id="name1" aria-describedby="emailHelp" placeholder="Enter name" />
+      <div>
+      <form onSubmit={sendData}>
+      <div>
+    <label>Name</label>
+    <input type="text" name="name" id="name1" aria-describedby="emailHelp" placeholder="Enter name" />
+  
   </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <div>
+    <label>Email address</label>
+    <input type="email" name="email" id="email1" aria-describedby="emailHelp" placeholder="Enter email" />
+  
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+  <div>
+    <label>Password</label>
+    <input type="password" name="password"  id="pass1" placeholder="Password" />
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <button type="submit">Submit</button>
 </form>
-        </div>
+      </div>
     </div>
   )
 }
